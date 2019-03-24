741d07eff18a424ec72f827522f8c1fb1553fb09
rodrigomartinscirqueira <rodrigomartinscirqueira@outlook.com>
2019-03-23 21:11:50 -0300

    App concluido

---
+++ AppCEP/AppCEP/AppCEP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using AppCEP.Model;
+using AppCEP.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = Cep.Text.Trim();
+            if(isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ServiceCEP.BuscaEnderecoViaCEP(cep);
+                    if (end.cep == null)
+                    {
+                        Resultado.Text = "CEP não existe";
+                    }
+                    else
+                    {
+                        Resultado.Text = string.Format("Endereço: {2} de {3} {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Erro critico", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                Resultado.Text = "CEP Inválido";
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            if (cep.Length != 8) return false;
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP)) return false;
+            return true;
+        }
+    }
+}
---
+++ AppCEP/AppCEP/AppCEP/Servico/ServiceCEP.cs
@@ -0,0 +1,26 @@
+﻿using AppCEP.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AppCEP.Servico
+{
+    public class ServiceCEP
+    {
+        public static string EnderecoUrl = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
